0d30ce4abf385972825e88572764bf9eaf96f918
Joe Fehrman <joseph.fehrman@gmail.com>
2019-11-21 20:04:58 -0500

    Creating method for creating a storybook file optionally.

--- plopfile.js
+++ plopfile.js
@@ -1,26 +1,46 @@
 module.exports = function(plop) {
   plop.setGenerator('component', {
     description: 'Create a functional react component',
-    prompts: [{
-      type: 'input',
-      name: 'name',
-      message: 'Component name please'
-    }],
-    actions: [{
-      type: 'add',
-      path: `${process.cwd()}/{{snakeCase name}}/{{snakeCase name}}.js`,
-      templateFile: 'plop-templates/component.hbs'
-    },
-    {
-      type: 'add',
-      path: `${process.cwd()}/{{snakeCase name}}/__test__/{{snakeCase name}}.test.js`,
-      templateFile: 'plop-templates/component.test.hbs'
-    },
-    {
-      type: 'add',
-      path: `${process.cwd()}/{{snakeCase name}}/{{snakeCase name}}.css`,
-      templateFile: 'plop-templates/component.css.hbs'
-    }]
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the component?'
+      },
+      {
+        type: 'confirm',
+        name: 'isStorybook',
+        message: 'Would you like to use storybook?'
+      }
+    ],
+    actions: function(data) {
+      const cwd = process.cwd();
+      let actions = [
+        {
+          type: 'add',
+          path: `${cwd}/{{snakeCase name}}/{{snakeCase name}}.js`,
+          templateFile: 'plop-templates/component.hbs'
+        },
+        {
+          type: 'add',
+          path: `${cwd}/{{snakeCase name}}/__test__/{{snakeCase name}}.test.js`,
+          templateFile: 'plop-templates/component.test.hbs'
+        },
+        {
+          type: 'add',
+          path: `${cwd}/{{snakeCase name}}/{{snakeCase name}}.css`,
+          templateFile: 'plop-templates/component.css.hbs'
+        }
+      ];
+      if (data.isStorybook)
+        actions.push({
+          type: 'add',
+          path: `${cwd}/{{snakeCase name}}/__test__/{{snakeCase name}}.story.js`,
+          templateFile: 'plop-templates/component.story.hbs'
+        });
+
+      return actions;
+    }
   });
 };
 
