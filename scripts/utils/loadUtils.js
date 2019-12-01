efda0cf0d8f4434d52f73be2e744b0b08855eedb
Joe Fehrman <joseph.fehrman@gmail.com>
2019-12-01 12:34:28 -0500

    Adding support for pulling preferences out of the eslintrc configuration.

--- scripts/utils/loadUtils.js
+++ scripts/utils/loadUtils.js
@@ -1,77 +1,87 @@
-const fs = require('fs');
+const fs = require('fs')
+const appRoot = require('app-root-path')
 
 const loadPackages = () => {
-  let Package = null;
-  try{
-    Package = JSON.parse(fs.readFileSync(`./package.json`, 'utf8'));
-    return Package;
+  let Package = null
+  try {
+    Package = JSON.parse(fs.readFileSync(`${appRoot.path}/package.json`, 'utf8'))
+    return Package
   } catch (e) {
-    throw new Error('You must have a package.json file initialized.' + e);
+    throw new Error(`You must have a package.json file initialized.${e}`)
   }
-};
+}
 
 const loadReactPreferences = () => {
-  let reactPreferences = null;
   try {
-    return JSON.parse(fs.readFileSync(`${appRoot.path}/react.preferences.json`, 'utf8'));
+    return JSON.parse(fs.readFileSync(`${appRoot.path}/react.preferences.json`, 'utf8'))
   } catch (e) {
-    return null;
+    return null
   }
-};
+}
 
-const checkIsTypescript = dependencies => 
-  Object.keys(dependencies)
-    .some(dependency => dependency === 'typescript');
+const loadEslint = () => {
+  try {
+    return JSON.parse(fs.readFileSync(`${appRoot.path}/.eslintrc.json`, 'utf8'))
+  } catch (e) {
+    return null
+  }
+}
 
-const checkIsPostcss = dependencies =>
-  Object.keys(dependencies)
-    .some(dependency => dependency === 'postcss');
+const checkIsTypescript = (dependencies) => Object.keys(dependencies)
+  .some((dependency) => dependency === 'typescript')
 
-const checkIsStorybook = dependencies =>
-  Object.keys(dependencies)
-    .some(dependency => dependency === '@storybook/cli');
+const checkIsPostcss = (dependencies) => Object.keys(dependencies)
+  .some((dependency) => dependency === 'postcss')
 
-const checkIsSass = dependencies =>
-  Object.keys(dependencies)
-    .some(dependency => dependency === 'node-sass');
+const checkIsStorybook = (dependencies) => Object.keys(dependencies)
+  .some((dependency) => dependency === '@storybook/cli')
 
-const checkIsJestInstalled = dependencies =>
-  Object.keys(dependencies)
-    .some(dependency => dependency === 'jest');
-;
+const checkIsSass = (dependencies) => Object.keys(dependencies)
+  .some((dependency) => dependency === 'node-sass')
+
+const checkIsJestInstalled = (dependencies) => Object.keys(dependencies)
+  .some((dependency) => dependency === 'jest')
+
+const checkIsJsx = ({ rules }) => Object.keys(rules)
+  .some((rule) => rule === 'react/jsx-filename-extension')
+  && rules['react/jsx-filename-extension'][1].extensions.indexOf('jsx')
+
+const checkIsSemicolon = (({ rules }) => !Object.keys(rules)
+  .some((rule) => rule === 'semi')
+  && rules.semi[0] === 2
+  && rules.semi[1] === 'never')
 
 const loadSettings = (data = {}) => {
-  const pkg = loadPackages();
-  const reactPreferences = loadReactPreferences();
+  const pkg = loadPackages()
+  const eslintConfig = loadEslint()
   const {
     devDependencies,
-    dependencies
-  } = pkg;
+    dependencies,
+  } = pkg
   const allPackages = {
     ...devDependencies,
-    ...dependencies
-  };
-
-  data.isTypescript = checkIsTypescript(allPackages);
-  data.isPostcss = checkIsPostcss(allPackages);
-  data.isStorybook = checkIsStorybook(allPackages);
-  data.isSass = checkIsSass(allPackages);
-  data.isJest = checkIsJestInstalled(allPackages);
-  if (reactPreferences) {
-    data.isJsx = reactPreferences.isJsx || false;
-    data.isSemicolons = reactPreferences.isSemicolons;
-    data.isSavePref = false;
+    ...dependencies,
   }
-  return data;
-};
+  /* eslint-disable no-param-reassign */
+  data.isTypescript = checkIsTypescript(allPackages)
+  data.isPostcss = checkIsPostcss(allPackages)
+  data.isStorybook = checkIsStorybook(allPackages)
+  data.isSass = checkIsSass(allPackages)
+  data.isJest = checkIsJestInstalled(allPackages)
+  data.isJsx = eslintConfig && checkIsJsx(eslintConfig)
+  data.isSemicolons = eslintConfig && checkIsSemicolon(eslintConfig)
+  return data
+}
 
 module.exports = {
   loadSettings,
   checkIsTypescript,
+  checkIsPostcss,
   checkIsStorybook,
   checkIsSass,
   checkIsJestInstalled,
   loadPackages,
   loadReactPreferences,
-};
+  loadEslint,
+}
 
