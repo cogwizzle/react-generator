347ce2a3e4c6fef1179f01046edd5acd24e20fc5
Joe Fehrman <joseph.fehrman@gmail.com>
2019-12-08 03:13:41 -0500

    Back testing the fileStructureUtils.

---
+++ scripts/utils/__test__/fileStructureUtils.test.js
@@ -0,0 +1,76 @@
+const {
+  getJsFileExtension,
+  getStyleSheetExtension,
+  generatePathWithPrefix,
+  extractPathAndComponentName,
+  getFileInfo,
+} = require('../fileStructureUtils.js')
+
+it('getJsFileExtension', () => {
+  const results = getJsFileExtension(false, false)
+  expect(results).toBe('js')
+})
+
+it('getJsFileExtension typescript', () => {
+  const results = getJsFileExtension(true, false)
+  expect(results).toBe('tsx')
+})
+
+it('getJsFileExtension jsx', () => {
+  const results = getJsFileExtension(false, true)
+  expect(results).toBe('jsx')
+})
+
+it('getStyleSheetExtension sass', () => {
+  const results = [
+    getStyleSheetExtension('scss'),
+    getStyleSheetExtension('sass'),
+  ]
+  expect(results.every((result) => result === 'scss')).toBeTruthy()
+})
+
+it('getStyleSheetExtension css', () => {
+  const results = [
+    getStyleSheetExtension('postcss'),
+    getStyleSheetExtension('post-css'),
+    getStyleSheetExtension('css'),
+  ]
+  expect(results.every((result) => result === 'css')).toBeTruthy()
+})
+
+it('generatePathWithPrefix non safe path', () => {
+  const results = generatePathWithPrefix('')
+  expect(results).toEqual('src/components/')
+})
+
+it('generatePathWithPrefix safe path', () => {
+  const results = generatePathWithPrefix('src/components/hello_world')
+  expect(results).toEqual('src/components/hello_world')
+})
+
+it('extractPathAndComponentName no path', () => {
+  const results = extractPathAndComponentName('hello_world')
+  expect(results).toEqual([
+    'src/components/',
+    'hello_world',
+  ])
+})
+
+it('extractPathAndComponentName with path', () => {
+  const results = extractPathAndComponentName('hello_world/another_one')
+  expect(results).toEqual([
+    'src/components/hello_world',
+    'another_one',
+  ])
+})
+
+it('getFileInfo', () => {
+  const results = getFileInfo('hello_world', true, false, 'postcss')
+  expect(results).toEqual([
+    'src/components/',
+    'hello_world',
+    'tsx',
+    'css',
+  ])
+})
+
--- scripts/utils/fileStructureUtils.js
+++ scripts/utils/fileStructureUtils.js
@@ -1,52 +1,46 @@
 const getJsFileExtension = (isTypescript, isJsx) => {
-  if (isTypescript)
-    return 'tsx';
-  else if (isJsx)
-    return 'jsx';
-  return 'js';
-};
+  if (isTypescript) return 'tsx'
+  if (isJsx) return 'jsx'
+  return 'js'
+}
 
 const getStyleSheetExtension = (styleType) => {
-  const lowerStyleType = styleType.toLowerCase();
+  const lowerStyleType = styleType.toLowerCase()
 
   switch (lowerStyleType) {
     case ('scss'):
     case ('sass'):
-      return 'scss';
-      break;
+      return 'scss'
     case ('postcss'):
     case ('post-css'):
     case ('css'):
     default:
-      return 'css';
-      break;
+      return 'css'
   }
-};
+}
 
 const generatePathWithPrefix = (path) => {
-  if(process.cwd().indexOf('src/components') < 0 
+  if (process.cwd().indexOf('src/components') < 0
     && path.indexOf('src/components') < 0) {
-    return `src/components/${path}`;
+    return `src/components/${path}`
   }
-  return path;
-};
+  return path
+}
 
 const extractPathAndComponentName = (componentName) => {
   if (componentName.indexOf('/') > -1) {
-    let [path, name] = componentName.split('/', -1);
-    return [generatePathWithPrefix(path), name];
+    const [path, name] = componentName.split('/', -1)
+    return [generatePathWithPrefix(path), name]
   }
-  return [generatePathWithPrefix(''), componentName];
+  return [generatePathWithPrefix(''), componentName]
 }
 
-const getFileInfo = (name, isTypescript, isJsx, styleType) => {
-  // Returns [path, name, jsExt, ssExt];
-  return [
-    ...extractPathAndComponentName(name),
-    getJsFileExtension(isTypescript, isJsx),
-    getStyleSheetExtension(styleType),
-  ];
-}
+const getFileInfo = (name, isTypescript, isJsx, styleType) => [
+  ...extractPathAndComponentName(name),
+  getJsFileExtension(isTypescript, isJsx),
+  getStyleSheetExtension(styleType),
+] // Returns [path, name, jsExt, ssExt]
+
 
 module.exports = {
   getJsFileExtension,
@@ -54,5 +48,5 @@ module.exports = {
   extractPathAndComponentName,
   getFileInfo,
   generatePathWithPrefix,
-};
+}
 
